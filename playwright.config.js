cf306af68f00bc5323cef3c446a5c0fe9a469f58
rajesh368071 <140796319+rajesh368071@users.noreply.github.com>
2025-04-24 16:29:57 +0530

    Update playwright.config.js
    Changes in fullyParallel as false and workers as 1

--- playwright.config.js
+++ playwright.config.js
@@ -15,13 +15,13 @@ const path = require("path");
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { outputFolder: path.join(__dirname, 'reports', `html-report-${moment().format('YYYYMMDD-HHmmss')}`), open: 'never' }],
